cae005e12d1140d90aa148be3922df5e7700c2dc
Eoin Davey <eoind@vey.ie>
2024-03-21 15:44:27 +0100

    New SDPA Parse file. Parses certificate

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ default:
 	@echo "empty"
 
 clean:
-	rm -r ./certificate ./data/ ./target/ ./*.sdpa ./report.html
+	rm -rf ./certificate ./data/ ./target/ ./*.sdpa ./report.html
--- sdpa_parse.py
+++ sdpa_parse.py
@@ -1,9 +1,15 @@
 import sys
+import os
 import numpy as np
+from collections import namedtuple
 from scipy.linalg import block_diag
 
-def parser():
-  lines = [line.strip() for line in sys.stdin]
+Parser = namedtuple('Parser', ['peek', 'eat', 'done'])
+
+def build_parser(fname):
+  with open(fname) as file:
+    lines = [line.strip() for line in file.read().split('\n')]
+
   def done():
     return len(lines) == 0
   def peek():
@@ -14,41 +20,85 @@ def parser():
     lines = lines[1:]
     return l
 
-  return peek, eat, done
+  return Parser(peek, eat, done)
 
-peek, eat, done = parser()
+if len(sys.argv) <= 1:
+  print('Missing args', file=sys.stderr)
+  sys.exit(1)
 
-while peek().startswith('*') or peek().startswith('"'):
-  eat()
+def read_meta(parser):
+  peek, eat, done = parser
 
-M = int(eat())
-NBlocks = int(eat())
-BlockStruct = [int(x) for x in eat().split()]
-C = [int(x) for x in eat().split()]
+  while peek().startswith('*') or peek().startswith('"'):
+    eat()
 
-fblks = []
-for _ in range(M+1):
-  blcks = []
-  for s in BlockStruct:
-    blcks.append(np.zeros((abs(s), abs(s))))
-  fblks.append(blcks)
+  m = int(eat())
+  n_blocks = int(eat())
+  block_struct = [int(x) for x in eat().split()]
 
-while not done():
-  ln = eat()
-  if ln == "":
-    continue
-  k, b, i, j, v = [int(x) for x in ln.split()]
-  if BlockStruct[b - 1] < 0:
-    assert(i == j)
-  fblks[k][b - 1][i - 1, j - 1] = v
-  fblks[k][b - 1][j - 1, i - 1] = v
+  return m, n_blocks, block_struct
 
-Fs = [block_diag(*bs) for bs in fblks]
+def read_body(parser, m, n_blocks, block_struct):
+  peek, eat, done = parser
 
-for bid in range(NBlocks):
-  print(f'Constraint {bid}:')
-  for i in range(1, M+1):
-    print(f'F_{i}')
-    print(fblks[i][bid])
-  print(f'F_0')
-  print(fblks[0][bid])
+  c = [float(x) for x in eat().split()]
+
+  fblks = []
+  for _ in range(m+1):
+    blcks = []
+    for s in block_struct:
+      blcks.append(np.zeros((abs(s), abs(s))))
+    fblks.append(blcks)
+
+  while not done():
+    ln = eat()
+    if ln == "":
+      continue
+    k, b, i, j = [int(x) for x in ln.split()[:-1]]
+    v = float(ln.split()[-1])
+    if block_struct[b - 1] < 0:
+      assert(i == j)
+    fblks[k][b - 1][i - 1, j - 1] = v
+    fblks[k][b - 1][j - 1, i - 1] = v
+
+  return c, fblks
+
+def print_body(c, blcks, m, n_blocks):
+  print(f'C: {c}')
+
+  fs = [block_diag(*bs) for bs in blcks]
+
+  for i in range(1, len(fs)):
+    print(f'F_{i}:\n{fs[i]}')
+  print(f'F_{0}:\n{fs[0]}')
+
+  #for bid in range(n_blocks):
+  #  print(f'Constraint {bid}:')
+  #  for i in range(1, m+1):
+  #    print(f'F_{i}')
+  #    print(blcks[i][bid])
+  #  print(f'F_0')
+  #  print(blcks[0][bid])
+
+def print_soln(c, blcks):
+  print(f'coefs:', [f'{x:.2f}' for x in c])
+
+  fs = [block_diag(*bs) for bs in blcks]
+  with np.printoptions(precision=3, suppress=True):
+    print(fs[2])
+
+def proc_problem():
+  parser = build_parser(sys.argv[1])
+
+  m, n_blocks, block_struct = read_meta(parser)
+  c, blcks = read_body(parser, m, n_blocks, block_struct)
+  print_body(c, blcks, m, n_blocks)
+
+  if len(sys.argv) < 3:
+    return
+
+  cert_parser = build_parser(sys.argv[2])
+  cert_c, cert_blcks = read_body(cert_parser, 2, n_blocks, block_struct)
+  print_soln(cert_c, cert_blcks)
+
+proc_problem()
